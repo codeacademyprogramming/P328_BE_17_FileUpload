3855f08d0fdf4871807d7fcc91bcf447bacfca25
Elchin Aghayev <Elchin@code.local>
2023-05-16 21:56:20 +0400

    project added

---
+++ P328Pustok/P328Pustok/Areas/Manage/Controllers/SliderController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P328Pustok.DAL;
+using P328Pustok.Helpers;
+using P328Pustok.Models;
+using P328Pustok.ViewModels;
+
+namespace P328Pustok.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class SliderController : Controller
+    {
+        private readonly PustokContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public SliderController(PustokContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index(int page=1)
+        {
+            var query = _context.Sliders.OrderBy(x=>x.Order).AsQueryable();
+
+            return View(PaginatedList<Slider>.Create(query,page,2));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Slider slider)
+        {
+            if (!ModelState.IsValid) return View();
+
+            if (slider.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "ImageFile is required");
+                return View();
+            }
+
+            if(slider.ImageFile.ContentType != "image/jpeg" && slider.ImageFile.ContentType != "image/png")
+            {
+                ModelState.AddModelError("ImageFile", "Content type must be jpeg,jpg or png");
+                return View();
+            }
+
+            if(slider.ImageFile.Length>2097152)
+            {
+                ModelState.AddModelError("ImageFile", "File size must be less than 2MB");
+                return View();
+            }
+
+            foreach (var item in _context.Sliders.Where(x=>x.Order>=slider.Order))
+            {
+                item.Order++;
+            }
+
+            slider.ImageName = FileManager.Save(_env.WebRootPath, "uploads/sliders", slider.ImageFile);
+
+            _context.Sliders.Add(slider);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Slider slider = _context.Sliders.Find(id);
+
+            if(slider == null) return View("Error");
+
+            return View(slider);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Slider slider)
+        {
+            if(!ModelState.IsValid) return View();
+
+            string fileName=null;
+            if (slider.ImageFile != null)
+            {
+                if (slider.ImageFile.ContentType != "image/jpeg" && slider.ImageFile.ContentType != "image/png")
+                {
+                    ModelState.AddModelError("ImageFile", "Content type must be jpeg,jpg or png");
+                    return View();
+                }
+
+                if (slider.ImageFile.Length > 2097152)
+                {
+                    ModelState.AddModelError("ImageFile", "File size must be less than 2MB");
+                    return View();
+                }
+
+
+                fileName = FileManager.Save(_env.WebRootPath, "uploads/sliders", slider.ImageFile);
+            }
+
+            Slider existSlider = _context.Sliders.Find(slider.Id);
+
+            if (existSlider == null) return View("Error");
+
+            existSlider.Order = slider.Order;
+            existSlider.Title1 = slider.Title1;
+            existSlider.Title2 = slider.Title2;
+            existSlider.BtnUrl = slider.BtnUrl; 
+            existSlider.BtnText= slider.BtnText;
+            existSlider.Desc = slider.Desc;
+            existSlider.ImageName = fileName??existSlider.ImageName;
+
+
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
---
+++ P328Pustok/P328Pustok/Models/Slider.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P328Pustok.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public int Order { get; set; }
+        [MaxLength(20)]
+        public string Title1 { get; set; }
+        [MaxLength(20)]
+        public string Title2 { get; set; }
+        [MaxLength(250)]
+        public string Desc { get; set; }
+        [MaxLength(50)]
+        public string BtnText { get; set; }
+        [MaxLength(250)]
+        public string BtnUrl { get; set; }
+        [MaxLength(100)]
+        public string ImageName { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+    }
+}
